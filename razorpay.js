436dcd8077e45c8098c711e6aee8ef4182724f93
bhalarambose2-dot <bhalarambose2@gmail.com>
2026-04-01 17:25:41 +0530

    Create razorpay.js

---
+++ razorpay.js
@@ -0,0 +1,19 @@
+window.startRazorpayPayment = function (amount, successCallback) {
+  const options = {
+    key: "YOUR_RAZORPAY_KEY_ID",
+    amount: amount * 100,
+    currency: "INR",
+    name: "HALORA",
+    description: "Payment",
+    handler: function (response) {
+      alert("Payment Successful: " + response.razorpay_payment_id);
+      if (successCallback) successCallback(response);
+    },
+    theme: {
+      color: "#0ea5e9"
+    }
+  };
+
+  const rzp = new Razorpay(options);
+  rzp.open();
+};

6e472c5c4ce9acdb620519259415befedb8b3454
bhalarambose2-dot <bhalarambose2@gmail.com>
2026-08-26 17:12:28 +0530

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,30 +1,23 @@
- // js/firebase-config.js
+// js/firebase-config.js
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-analytics.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDJnOh92AYUzFeWtuLMtDciETdpCQ7-MNs",
+  apiKey: "अपनी मौजूदा API KEY यहां रखो",
   authDomain: "halorebook.firebaseapp.com",
   projectId: "halorebook",
   storageBucket: "halorebook.firebasestorage.app",
-  messagingSenderId: "58132767978",
-  appId: "1:58132767978:web:8136b579841652b15a6393",
-  measurementId: "G-TDSM97W54X"
+  messagingSenderId: "अपनी मौजूदा Messaging Sender ID यहां रखो",
+  appId: "अपनी मौजूदा App ID यहां रखो"
 };
 
 const app = initializeApp(firebaseConfig);
+
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
 
-// Analytics sirf browser me hi chale
-let analytics = null;
-if (typeof window !== "undefined") {
-  analytics = getAnalytics(app);
-}
-
-export { app, auth, db, storage, analytics };
+export { app, auth, db, storage };
